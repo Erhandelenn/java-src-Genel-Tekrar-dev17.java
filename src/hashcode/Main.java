445eb8d6ee36dac1b58534b4a4f604e3eee40814
Erhan Delen <Erhandln611@gmail.com>
2024-07-02 16:12:17 +0300

    güncelleme

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Set<Student> studentList = new HashSet<>();
         studentList.add(new Student("Erhan", "Delen", 20, "123", 3.5));
         studentList.add(new Student("Yekta", "Karatay", 21, "124", 3.8));
-        studentList.add(new Student("Esra", "Delen", 22, "123", 3.9)); // Aynı okul numarasına sahip, önceki değeri üzerine yazılacak
+        studentList.add(new Student("Esra", "Delen", 22, "123", 3.9));
         studentList.add(new Student("Fatma", "Demir", 23, "125", 3.6));
 
         for (Student student : studentList) {
